a2f9dbc2ae90c21232289be07c1e8af13b5e3f03
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-17 15:44:18 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,13 +17,6 @@ resource "aws_security_group" "focal_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  
-    ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
