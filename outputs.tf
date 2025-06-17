30ca076c713b693c005ff85c6cf0b07fdc467619
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-17 15:22:15 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,1 @@
-module "ec2" {
-  source         = "./modules/ec2"
 
-output "ec2_id" {
-value = aws_instance.focal_ec2.id
-}
- 
-output "security_group_id" {
-value = aws_security_group.focal_sg.id
-}
