51c3c9770419312bca7044181009290685f53bba
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-17 14:42:38 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "ec2" {
+  source         = "./modules/ec2"
+  ami_id         = var.ami_id
+  instance_type  = var.instance_type
+  key_name       = var.key_name
+  vpc_id         = module.vpc.vpc_id
+  subnet_ids     = module.vpc.public_subnet_ids
+}
