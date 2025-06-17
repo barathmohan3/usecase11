926092335973f084d1644153ae7c92b9ca616c58
barathmohan3 <45123296+barathmohan3@users.noreply.github.com>
2025-06-17 14:42:54 +0530

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+ 
+variable "ami_id" {
+  default = "ami-0c7217cdde317cfec"
+}
+ 
+variable "instance_type" {
+  default = "t2.micro"
+}
+ 
+variable "key_name" {
+  default = "first"
+}
